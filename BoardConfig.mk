9a1f4ce3c2fb26d7e7418647fa398d4013913121
Jrcable1 <jrcable1@gmail.com>
2024-11-29 02:57:46 -0500

    gta4xlwifi: override system properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,13 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+DEVICE_PATH := device/samsung/gta4xlwifi
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_gta4xlwifi
+
 ## Inherit from the common tree
 include device/samsung/gta4xl-common/BoardConfigCommon.mk
 
 ## Inherit from the proprietary configuration
 include vendor/samsung/gta4xlwifi/BoardConfigVendor.mk
-
-DEVICE_PATH := device/samsung/gta4xlwifi
 
 ## Kernel
 TARGET_KERNEL_CONFIG := exynos9611-gta4xlwifi_defconfig
